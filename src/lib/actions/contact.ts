96d7ad95b08e04335556e2c52acc4b1695ec252f
sato <ss.rin.3103@gmail.com>
2025-09-28 23:04:28 +0900

    4-35まで完了

---
+++ src/lib/actions/contact.ts
@@ -0,0 +1,14 @@
+'use server'
+import { redirect } from "next/navigation"
+
+export async function submitContactForm(formData: FormData) {
+    const name = formData.get('name')
+    const email = formData.get('email')
+
+    //バリデーション
+    //DB
+
+    console.log('送信されたデータ：',{name, email})
+    redirect('/contacts/complete')
+
+}
