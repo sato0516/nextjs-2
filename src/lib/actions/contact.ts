a92fbc2de3abb8a995dda30f56a7f1ab0130df83
sato <ss.rin.3103@gmail.com>
2025-09-29 17:39:14 +0900

    4-37まで完了

--- src/lib/actions/contact.ts
+++ src/lib/actions/contact.ts
@@ -2,16 +2,35 @@
 import { redirect } from 'next/navigation'
 import { ContactSchema } from '@/validations/contact'
 
-export async function submitContactForm(formData: FormData) {
+//ActionStateの型定義
+type ActionState = {
+    success: boolean;
+    errors: {
+        name?: string[];
+        email?: string[];
+    };
+    serverError?: string;
+};
+
+export async function submitContactForm(
+    prevState: ActionState,
+    formData: FormData):Promise<ActionState> {
     const name = formData.get('name')
     const email = formData.get('email')
 
     //バリデーション
     const validationResult = ContactSchema.safeParse({name, email})
     if(!validationResult.success){
-        const errors = validationResult.error.flatten();
+        const errors = validationResult.error.flatten().fieldErrors;
         console.log('サーバー側でエラー',errors);
-        return {};
+        return {
+            success: false,
+            errors:
+            {
+                name: errors.name || [],
+                email: errors.email || []
+            }
+        };
     }
 
     //DB
