40136cba73f359aee2b365f2170de5261e54a298
sato <ss.rin.3103@gmail.com>
2025-10-11 12:37:03 +0900

    マイグレーションファイルをpostgreに変更

---
+++ prisma/migrations/20251011033530_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "public"."Contact" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Contact_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Contact_email_key" ON "public"."Contact"("email");
