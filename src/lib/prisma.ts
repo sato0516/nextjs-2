cc0feffa90ff933e98bb550ae56456a8db08f1ef
sato <ss.rin.3103@gmail.com>
2025-12-02 18:01:02 +0900

    エラー解消_Prisma Client 生成場所の修正

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,11 +1,14 @@
-import { PrismaClient } from '@/generated/prisma'
+//import { PrismaClient } from '@/generated/prisma'
+import { PrismaClient } from "@prisma/client";
 
 // グローバルスコープでPrismaインスタンスを保持できる場所を作る
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined
+  prisma?: PrismaClient;
 }
 
 // Prismaインスタンスがあれば使う、なければ作成
 export const prisma = globalForPrisma.prisma ?? new PrismaClient();
 // 開発環境でのみ使用
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
+}
